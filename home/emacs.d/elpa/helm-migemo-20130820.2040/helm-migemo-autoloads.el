89beca169358d2c5c4703ad3854485d5817c7cec
Takuto Komazaki <komazarari@gmail.com>
2014-02-16 15:08:54 +0900

    remove emacs.d

--- home/emacs.d/elpa/helm-migemo-20130820.2040/helm-migemo-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; helm-migemo-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("helm-migemo-pkg.el" "helm-migemo.el")
-;;;;;;  (21103 23761 437349 0))
-
-;;;***
-
-(provide 'helm-migemo-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; helm-migemo-autoloads.el ends here
